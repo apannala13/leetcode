e9e8798dda124af51aaa65ce90314b9ea57dc30b
apannala13 <145242587+apannala13@users.noreply.github.com>
2024-10-13 22:55:48 -0700

    add hashmap

--- two_ptr/two_sum_ii.cpp
+++ two_ptr/two_sum_ii.cpp
@@ -19,3 +19,22 @@ public:
         return {};
     }
 };
+
+
+
+
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& numbers, int target) {
+//         unordered_map<int, int> hashmap;
+
+//         for(int i = 0; i < numbers.size(); ++i){
+//             int diff = target - numbers[i];
+//             if(hashmap.find(diff) != hashmap.end()){
+//                 return {hashmap[diff] + 1, i + 1};
+//             }
+//             hashmap[numbers[i]] = i;
+//         }
+//         return {};
+//     }
+// };
